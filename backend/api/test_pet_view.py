a09576d3cf66d6d61b4b255ff5a4d80ebc767e90
unknown <arix69sex@gmail.com>
2024-08-10 01:27:10 -0500

    feat: Clean test pet view

--- backend/api/test_pet_view.py
+++ backend/api/test_pet_view.py
@@ -1,7 +1,7 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from api.models import PetModel, AdoptionModel, UserModel
+from api.models import PetModel
 
 class PetViewTestCase(APITestCase):
 
