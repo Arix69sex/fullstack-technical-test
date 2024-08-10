449acb9f61f66ae374015a9c51fb7db646e97e98
unknown <arix69sex@gmail.com>
2024-08-10 01:37:42 -0500

    feat: Initial frontend config with react and mantine ui

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Container, Title } from '@mantine/core';
+
+function App() {
+  return (
+    <Container>
+      <Title order={1}>Welcome to Mantine with React!</Title>
+      <Button>Hello Mantine Button</Button>
+    </Container>
+  );
+}
+
+export default App;
