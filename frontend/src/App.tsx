5db8c0f5fb7f7031da2a10ad3fcd496fe9631cad
unknown <arix69sex@gmail.com>
2024-08-10 18:44:09 -0500

    feat: Initial configuration for react + mantine ui + typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mantine/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant = "filled">Button</Button>
+    </div>
+  );
+}
+
+export default App;
