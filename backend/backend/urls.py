0001474917c3fbde78e219ee1a0ce0d30c90cc03
unknown <arix69sex@gmail.com>
2024-08-13 02:29:25 -0500

    fix: Fix old file imports

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,7 @@ from api.custom_token_obtain_view import CustomTokenObtainPairView
 
 urlpatterns = [
     path("admin", admin.site.urls),
-    path("api/", include("api.adapters.urls")),
+    path("api/", include("api.urls")),
     path('api/auth/login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/register', UserView.as_view(), name='token_obtain_pair'),
